2cec8b9a8a06140c6e085a098b1382c24dc13f0b
unknown <abderahmanamarnis333@gmail.com>
2023-10-17 11:55:15 +0100

    game

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-
+var dr = document.querySelector(".drop") 
 
 function abd(ed){
    ed.preventDefault();
@@ -9,33 +9,125 @@ function drag(ed){
 function drop(ed){
    ed.preventDefault();
    var data = ed.dataTransfer.getData("text") 
-   if (ed.target.innerHTML == "FALSE" || ed.target.innerHTML =="CORRECT"  || ed.target.innerHTML =="KAYN KAYN" ){
+   if (ed.target.innerHTML == "FALSE" || ed.target.innerHTML =="CORRECT"  || ed.target.innerHTML =="KAYN KAYN" || ed.target.innerHTML =="EMPTY"){
       ed.target.innerHTML = ""
    }
    // ed.target.appendChild(document.getElementById(data))
    ed.target.innerHTML+= data 
+
+}
+const arr = ["BAT","GOAT","DRAGON"]
+let i = 0
+const ob = {
+   BAT : "🐐", GOAT  : "🐲" , DRAGON : "10/10"
+}
+function next(){
+   if (dr.innerHTML == "CORRECT"){
+
+      document.getElementById("em").innerHTML= ob[arr[i]]
+      i++
+      if (i>= arr.length){
+         i=0
+        
+      }
+       dr.innerHTML= ""
+}
 }
 function corr(){
-   var dr = document.querySelector(".drop") 
-   // if (dr.innerHTML =="BAT" ){
-   //   dr.innerHTML = "CORRECT"
-   // }
-   // else{
-   //    dr.innerHTML = "FALSE"
-   // }
-switch (dr.innerHTML ){
-   case "BAT":
+   if (dr.innerHTML in ob ){
+      // document.getElementById("em").innerHTML=ob[dr.innerHTML]
       dr.innerHTML = "CORRECT"
-      break;
-      case "BATATA":
-      dr.innerHTML = "KAYN KAYN"
-      break;
-      default : 
-      dr.innerHTML = "FALSE"
-
+   //   emoji.textContent= "🦍"
+   //   if (dr.innerHTML == "CORRECT"){
+   //     var bat = document.getElementById("em")
+   //   bat.innerHTML = ""
+   //   var emoji = document.createElement("span")
+   //   emoji.textContent= "🦍"
+   //   var emoji2 = document.getElementById("em")
+   //   emoji2.appendChild(emoji)
+   //   }
+   
+   
+   
+   //    } if (dr.innerHTML =="BA" ){
+//       dr.innerHTML = "CORRECT"
+//       if (dr.innerHTML == "CORRECT"){
+//         var bat = document.getElementById("em")
+//       bat.innerHTML = ""
+//       var emoji = document.createElement("span")
+//       emoji.textContent= "🐲"
+//       var emoji2 = document.getElementById("em")
+//       emoji2.appendChild(emoji)
+//       }
+     
+    
+// }
+// if (dr.innerHTML =="B" ){
+//    dr.innerHTML = "CORRECT"
+//    if (dr.innerHTML == "CORRECT"){
+//      var bat = document.getElementById("em")
+//    bat.innerHTML = ""
+//    var emoji = document.createElement("span")
+//    emoji.textContent= "🐐"
+//    var emoji2 = document.getElementById("em")
+//    emoji2.appendChild(emoji)
+//    }
+// }
 
    
-
    
-} 
-}
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   }else{
+      dr.innerHTML ="FALSE"
+     }}
+
+
+
+//    } if (dr.innerHTML =="BA" ){
+//       dr.innerHTML = "CORRECT"
+//       if (dr.innerHTML == "CORRECT"){
+//         var bat = document.getElementById("em")
+//       bat.innerHTML = ""
+//       var emoji = document.createElement("span")
+//       emoji.textContent= "🐲"
+//       var emoji2 = document.getElementById("em")
+//       emoji2.appendChild(emoji)
+//       }
+     
+    
+// }
+// if (dr.innerHTML =="B" ){
+//    dr.innerHTML = "CORRECT"
+//    if (dr.innerHTML == "CORRECT"){
+//      var bat = document.getElementById("em")
+//    bat.innerHTML = ""
+//    var emoji = document.createElement("span")
+//    emoji.textContent= "🐐"
+//    var emoji2 = document.getElementById("em")
+//    emoji2.appendChild(emoji)
+//    }
+// }
+
